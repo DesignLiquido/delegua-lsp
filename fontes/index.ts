fbb1a3e5e9a2ae2edef7cea063fd2803a867b53f
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2026-07-26 11:33:57 -0700

    Atualização do núcleo de Delégua e colaterais.

--- fontes/index.ts
+++ fontes/index.ts
@@ -3,6 +3,7 @@ export { executarAnalises } from './analisador';
 export { proverItensCompletude, proverDefinicao, proverReferencias, prepareRename, provideRenameEdits } from './capacidades';
 export * from './interfaces/ambiente-lsp-interface';
 export * from './interfaces/documento-lsp-interface';
+export * from './interfaces/entrada-diretorio-interface';
 export * from './interfaces/manipulador-caminhos-interface';
 export * from './interfaces/resultado-analise-interface';
 export * from './interfaces/sistema-arquivos-interface';
